c382bdae95f7fbe5d5a2c609cf299bcb68109510
riniba <pulverivqcdl@gmail.com>
2026-01-16 00:16:24 +0800

    移除无用字段

--- src/Options/TelegramOptions.cs
+++ src/Options/TelegramOptions.cs
@@ -5,7 +5,4 @@ public class TelegramOptions : IConfigurableOptions
     public int ApiId { get; set; }
     public string ApiHash { get; set; }
     public string BotToken { get; set; }
-    public bool EnabledLogging { get; set; }
-    public string LogPath { get; set; }
-    public string LogName { get; set; }
 }
